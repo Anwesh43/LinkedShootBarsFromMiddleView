fe2e19c8e9cb5f0ba88c4b49177d66a8e0011e4f
Anwesh43 <anweshthecool0@gmail.com>
2020-08-22 03:48:28 +0530

    adding renderer in view

--- shootbarsfrommiddleview/src/main/java/com/anwesh/uiprojects/shootbarsfrommiddleview/ShootBarsFromMiddleView.kt
+++ shootbarsfrommiddleview/src/main/java/com/anwesh/uiprojects/shootbarsfrommiddleview/ShootBarsFromMiddleView.kt
@@ -50,14 +50,16 @@ fun Canvas.drawBFMNode(i : Int, scale : Float, paint : Paint) {
 
 class ShootBarsFromMiddleView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
