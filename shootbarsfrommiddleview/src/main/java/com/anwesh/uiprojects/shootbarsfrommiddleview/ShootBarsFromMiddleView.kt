62faa757d4761fa138fd1d23029018ec40e95382
Anwesh43 <anweshthecool0@gmail.com>
2020-08-22 03:43:04 +0530

    adding SBFMNode class

--- shootbarsfrommiddleview/src/main/java/com/anwesh/uiprojects/shootbarsfrommiddleview/ShootBarsFromMiddleView.kt
+++ shootbarsfrommiddleview/src/main/java/com/anwesh/uiprojects/shootbarsfrommiddleview/ShootBarsFromMiddleView.kt
@@ -110,4 +110,45 @@ class ShootBarsFromMiddleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBFMNode(var i : Int, val state : State = State()) {
+
+        private var next : SBFMNode? = null
+        private var prev : SBFMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBFMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBFMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBFMNode {
+            var curr : SBFMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
